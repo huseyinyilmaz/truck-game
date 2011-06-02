c7316a9cb7091472b693ac5ec639ea4f178eeab3
huseyin <huseyin.yilmaz@markafoni.com>
2011-06-02 16:51:28 +0300

    started to implement config menu

--- main.js
+++ main.js
@@ -9,16 +9,34 @@ window.game = {
     ballMiddle : 375,
     balldifference : 150,
     balls:[],
+    applyConfig:function(color,count){
+	this.currentColor = color;
+	this.ballCount=count;
+    },
     initConfig:function(element){
-
+	var that = this;//for dialog onClick event
+	this.configDialog = $(element).dialog({ autoOpen: false,
+						title: 'Game Configuration',
+						buttons:{save:function(){
+							     that.applyConfig();
+							     game.configDialog.dialog('close');
+							     alert('apply Changes');}},
+            		    closeOnEscape:true,
+      			    modal:true
+			    });
     }
 };
+			 
+
 
 window.onload=function(){
     game.svg = document.getElementById('svgContainer');
     var balls = ['red','green','blue','yellow'];
-
     game.initTruck();
     game.initializeballs(balls);
     game.truck.move(600,0,'3s');
+    var configDialogElement = document.getElementById('configDialog');
+    game.initConfig(configDialogElement);
+    game.configDialog.dialog('open');
+
 };
